e15af065204f4f1c4394b4a557afa5660e8b81c7
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-27 21:42:19 +0530

    made the changes

---
+++ mysql/tran.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS crud_app;
+USE crud_app;
+
+CREATE TABLE IF NOT EXISTS product (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    quantity INT NOT NULL
+);
+
+CREATE USER IF NOT EXISTS 'crud'@'%' IDENTIFIED BY 'CrudApp@1';
+GRANT ALL ON crud_app.* TO 'crud'@'%';
+FLUSH PRIVILEGES;
