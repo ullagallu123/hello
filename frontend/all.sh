da6b90b2a30b9d9c00ac37118b75d7d414a9a551
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-28 16:40:42 +0530

    made the changes

--- frontend/all.sh
+++ frontend/all.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 if [ -z "${BACKEND_HOST}" ]; then
-  echo Input BACKEND_HOST is missing
+  echo "Input BACKEND_HOST is missing"
   exit 1
 fi
 
 if [ -z "${BACKEND_PORT}" ]; then
-  echo Input BACKEND_PORT is missing
+  echo "Input BACKEND_PORT is missing"
   exit 1
 fi
 
-cat /opt/nginx.conf | envsubst >/etc/nginx/nginx.conf
-nginx -g 'daemon off;'
+envsubst < /opt/nginx.conf > /etc/nginx/nginx.conf
 
+nginx -g 'daemon off;'
